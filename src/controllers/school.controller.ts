a96135c207612bed3417d80a388c840d75316e7b
khabubundivhu <khabubundivh@gmail.com>
2021-10-25 23:15:25 +0200

    Add pagination

--- src/controllers/school.controller.ts
+++ src/controllers/school.controller.ts
@@ -2,13 +2,49 @@ import { Response, Request } from 'express';
 import ISchool from '../interfaces/ISchool';
 import SchoolModel from '../models/School';
 
+const getPagination = (page: number, size: number) => {
+	const limit = size ? +size : 3;
+	const offset = page ? page * limit : 0;
+
+	return { limit, offset };
+};
+
+const getPagingData = (
+	data: ISchool[],
+	count: number,
+	page: number,
+	limit: number
+) => {
+	const currentPage = page ? +page : 0;
+	const totalPages = Math.ceil(count / limit);
+
+	return {
+		totalItems: count,
+		schools: data,
+		totalPages: totalPages - 1,
+		currentPage,
+	};
+};
+
 const getSchools = async (req: Request, res: Response) => {
 	try {
-		const fetchSchools = await SchoolModel.findAll();
+		const { page } = req.query;
+
+		const setPage = Number(page) || 1;
+		const pageSize = 10;
+
+		const { limit, offset } = getPagination(setPage, pageSize);
+
+		const fetchSchools = await SchoolModel.findAndCountAll({
+			limit,
+			offset,
+		});
+
+		const { rows, count } = fetchSchools;
 
 		const schoolsArr: ISchool[] = [];
 
-		fetchSchools.forEach((school) => {
+		rows.forEach((school) => {
 			schoolsArr.push({
 				id: school.id,
 				name: school.name,
@@ -19,9 +55,16 @@ const getSchools = async (req: Request, res: Response) => {
 			});
 		});
 
+		const pagingData = getPagingData(schoolsArr, count, setPage, limit);
+
+		// sort schoolsArr alphabetically
+		// const formatedData = schoolsArr.sort((a, b) =>
+		// 	a.name > b.name ? 1 : -1
+		// );
+
 		return res.status(200).json({
 			success: true,
-			data: schoolsArr,
+			data: pagingData,
 		});
 	} catch (error) {
 		return res.status(500).json({
