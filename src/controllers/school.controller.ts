6337ddea2beb5788e80d0529577247901d9c86c4
khabubundivhu <khabubundivh@gmail.com>
2021-10-26 01:08:33 +0200

    Added: import data function

--- src/controllers/school.controller.ts
+++ src/controllers/school.controller.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from 'express';
+import schoolData from '../data/schools.json';
 import ISchool from '../interfaces/ISchool';
 import SchoolModel from '../models/School';
 
@@ -36,6 +37,41 @@ const getSchools = async (req: Request, res: Response) => {
 	}
 };
 
+const importData = async (req: Request, res: Response) => {
+	try {
+		const schools: ISchool[] = [];
+		schoolData.forEach(async (school) => {
+			const newSchool = await SchoolModel.create({
+				name: school.name,
+				address: school.address,
+				latitude: school.latitude,
+				longitude: school.longitude,
+				image_link:
+					'https://icon-library.com/images/school-icon-transparent/school-icon-transparent-4.jpg',
+			});
+			schools.push({
+				id: newSchool.id,
+				name: newSchool.name,
+				address: newSchool.address,
+				latitude: newSchool.latitude,
+				longitude: newSchool.longitude,
+				imageLink: newSchool.image_link,
+			});
+		});
+
+		return res.status(200).json({
+			success: true,
+			data: schools,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			error: 'Something went wrong',
+		});
+	}
+};
+
 export default {
 	getSchools,
+	importData,
 };
--- src/routes/school.routes.ts
+++ src/routes/school.routes.ts
@@ -4,5 +4,6 @@ import schoolController from '../controllers/school.controller';
 const router = express.Router();
 
 router.get('/', schoolController.getSchools);
+router.get('/import-data', schoolController.importData);
 
 export default router;
